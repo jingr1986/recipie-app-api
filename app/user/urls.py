41e4f6f785fb3716dfd9004f4575e0862b75c61c
jingr1986 <jingr1986@gmail.com>
2020-02-15 16:51:45 +1100

    added endpoint for creating users

---
+++ app/user/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from user import views
+
+app_name = 'user'
+
+urlpatterns = [
+    path('token/', views.CreateTokenView.as_view(), name='token'),
+    path('create/', views.CreateUserView.as_view(), name='create'),
+]
